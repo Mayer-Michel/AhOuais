35eaa2dff3c2c06c04727d9d08d6a34572dfedfa
Mayer MICHEL <mayer.mohib@gmail.com>
2024-12-06 15:51:16 +0100

    modification de gitignore

--- app/www/index.php
+++ app/www/index.php
@@ -1,2 +1,19 @@
-<?php 
-phpinfo();
+<?php
+
+use App\App;
+use Dotenv\Dotenv;
+
+// Définir la constante DIRECTORY_SEPARATOR en DS
+const DS = DIRECTORY_SEPARATOR;
+// Chemin physique vers le dossier racine 
+define('ROOT_PATH', dirname(__FILE__, 2). DS);
+// Chemin physique vers le dossier "src
+define('APP_PATH', ROOT_PATH .'src'. DS);
+
+// Chargement du sytéme d'autoload
+require_once ROOT_PATH. DS .'vendor'. DS .'autoload.php';
+
+// Chargement du module .env
+Dotenv::createImmutable( ROOT_PATH )->load();
+
+App::getApp()->start();
